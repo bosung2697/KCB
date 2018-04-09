e0d8067bd09515d4a5f9f5607ddfdb5760f6018c
bosung2697 <36359088+bosung2697@users.noreply.github.com>
2018-04-09 15:38:37 +0900

    last friday

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="navigationheader">
-    <img src=" " title="logo" alt="logo" align="left" style="margin-left: 14vw;">
+    <a href="/home"><img src=" " title="logo" alt="logo" align="left" style="margin-left: 14vw;"></a>
     <ul>
-        <li><a href="">로그인</a></li>
-        <li><a href="">회원가입</a></li>
+        <li><a href="/login">로그인</a></li>
+        <li><a href="/register">회원가입</a></li>
         <li><a href="">공지사항</a></li>
         <li><a href="">고객센터</a></li>
         <li><div class="dropdown">
@@ -33,7 +33,7 @@
         </form>
     </div>
     <div>
-        <a href=""><p class="navbarotherpage">TODAY 보상 공고/공시</p></a>
-        <a href=""><p class="navbarotherpage">보상/용역 대행 컨설팅</p></a>
+          <a href=""><p class="navbarotherpage">TODAY 보상 공고/공시</p></a>
+          <a href=""><p class="navbarotherpage">보상/용역 대행 컨설팅</p></a>
     </div>
 </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,5 +5,9 @@
     <div class="grid-item">사이트맵</div>
     <div class="grid-item">회사 위치</div>
     <div class="grid-item">회사 소개</div>
-
+</div>
+<div class="footerextra">
+    <p>copyright 보상원 2018</p>
+    <p>주소 경기도 성남시 중원구 광명로 377, 신구대학교 창업관 507호 | 전화 000-0000-0000 | 팩스 000-0000-0000</p>
+    <p>사업자번호 00000000000</p>
 </div>
